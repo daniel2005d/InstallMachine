5038573bd79bd22da3af3bb0a8684fc367392d27
Daniel <daniel2005d@gmail.com>
2020-03-12 15:27:27 -0500

    Se adicionan los archivos de configuracion del entorno

---
+++ .config/bin/vpnstatus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
+if [ "$IFACE" = "tun0" ]; then
+	echo "%{F#1bbf3e} %{F#e2ee6a}$(/usr/sbin/ifconfig tun0 | grep "inet" | awk '{print $2}')%{u-}"
+else
+	echo "%{F#1bbf3e}%{u-}%{F-}"
+fi
