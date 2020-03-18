81f44a7d813d74abbef1d953cc21b251c155af2b
Daniel <daniel2005d@gmail.com>
2020-03-18 13:37:20 -0500

    Update

--- .config/bin/vpnstatus.sh
+++ .config/bin/vpnstatus.sh
@@ -3,6 +3,6 @@
 IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
 if [ "$IFACE" = "tun0" ]; then
 	echo "%{F#1bbf3e} %{F#e2ee6a}$(/usr/sbin/ifconfig tun0 | grep "inet" | awk '{print $2}')%{u-}"
-else
-	echo "%{F#1bbf3e}%{u-}%{F-}"
+#else
+#	echo "%{F#1bbf3e}%{u-}%{F-}"
 fi
